4780490c7e075f2b1c7ebf1db38842db52b44a27
“techxxy” <jeeyoung.park.5@gmail.com>
2024-02-25 13:52:03 +0100

    apply output export to generate static pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    images: {
+        unoptimized: true,
+      },
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
   })
    
-  module.exports = withNextra()
+  module.exports = {
+    ...withNextra(),
+    ...nextConfig,
+  };

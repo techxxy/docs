8365ba0b4da27ee4ff366d0824dfd31b0ec285ab
“techxxy” <jeeyoung.park.5@gmail.com>
2024-02-25 13:30:53 +0100

    start from Nextra hello worlde

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+    logo: <span>My Nextra Documentation</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    }
+    // ... other theme options
+  }
+  
+  

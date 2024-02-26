30bbdca3c89c6f78b69b507d37c9b043f6bdd9c5
“techxxy” <jeeyoung.park.5@gmail.com>
2024-02-26 01:12:18 +0100

    add new post

--- next.config.js
+++ next.config.js
@@ -7,20 +7,12 @@ const nextConfig = {
       },
     basePath: '/documentation', 
     output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
   }
 
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
+    //staticImage: true
   })
    
   module.exports = {
